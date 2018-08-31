6ebb3b360edef82d7ca5100ed81099d35deacf66
DarkEye <lesko.matej.pu@gmail.com>
2018-09-01 01:55:54 +0200

    Animations added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row justify-content-center align-items-center">
-        <h1>Welcome to Simple Math done!</h1>
+        <div class="row justify-content-center align-items-center">
+          <h1>Welcome to Simple Math done!</h1>
+        </div>
+        <hr>
+        <transition mode="out-in" :enter-active-class="selectedComponent != 'math-solver' ? 'animated tada' : 'animated fadeIn'" leave-active-class="animated fadeOut" appear appear-active-class="animated bounceIn">
+          <component :is="selectedComponent"
+          :maxRows=2
+          :maxColumns=2
+          :maxRandVal=100
+          @correctSelected="showSuccessComponent"
+          @startNewGame="startNewGame"></component>
+        </transition>
       </div>
-      <hr>
-      <transition>
-        <component :is="selectedComponent"
-        :maxRows=2
-        :maxColumns=2
-        :maxRandVal=100
-        @correctSelected="showSuccessComponent"
-        @startNewGame="startNewGame"></component>
-      </transition>
-    </div>
   </div>
 </template>
 
